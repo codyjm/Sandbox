5a40b2e78fba6cb8e5281f33576ac9de7d94be92
codyjm <cody.moxham@my.jcu.edu.au>
2016-08-24 14:21:52 +1000

    add name printing program

--- oddName.py
+++ oddName.py
@@ -1,3 +1,11 @@
 """
 Cody Moxham
+print every second letter of name
 """
+name = input("Enter name: ")
+while len(name) == 0:
+    name = input("Enter a name: ")
+
+for i in range(len(name)):
+    if i % 2 == 1:
+        print(name[i])
